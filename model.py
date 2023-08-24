50335ff9dbc0f7343f81a4b9d6537041ee1f0ba9
astroboy890506 <97178858+astroboy890506@users.noreply.github.com>
2023-08-24 15:32:59 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,8 +1,3 @@
-pip install pandas
-pip install sklearn
-pip install joblib
-pip install streamlit
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
